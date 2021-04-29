01aa0e7e97a892ce221997de31a437ea0cf4c496
EliseevG787 <rdn787@bk.ru>
2021-04-29 03:44:42 +0300

    added /LoginSystemTest.java

--- src/test/java/ru/netology/web/test/LoginSystemTest.java
+++ src/test/java/ru/netology/web/test/LoginSystemTest.java
@@ -3,62 +3,65 @@ package ru.netology.web.test;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.netology.DbInteraction;
+import ru.netology.web.data.DbInteraction;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPage;
-
-import java.sql.SQLException;
 
 import static com.codeborne.selenide.Selenide.open;
 
 class LoginSystemTest {
     @BeforeEach
-    void setUp() throws SQLException {
+    void setUp() {
         DbInteraction.clearDatabase();
         open("http://localhost:9999");
     }
 
+    String login = DataHelper.getAuthInfo().getLogin();
+    String password = DataHelper.getAuthInfo().getPassword();
+    String invalidLogin = DbInteraction.getInvalidLogin();
+    String invalidPassword = DbInteraction.getInvalidPassword();
+
     @Test
-    void shouldLoginToTheSystem() throws SQLException {
+    void shouldLoginToTheSystem() {
         val loginPage = new LoginPage();
-        val verificationPage = loginPage.validLogin();
-        String code = DbInteraction.verificationCode();
+        val verificationPage = loginPage.validLogIn(login, password);
+        val code = DbInteraction.verificationCode();
         verificationPage.validVerify(code);
     }
 
     @Test
-    void shouldIncorrectPasswordEnteredThreeTimes() {
+    void shouldSystemIsBlocked() {
         val loginPage = new LoginPage();
-        loginPage.invalidPassword();
-        val verificationPage = loginPage.validLogin();
-        verificationPage.verificationPageIsHidden();
+        loginPage.invalidLogIn(login, invalidPassword);
+        loginPage.invalidLogIn(login, invalidPassword);
+        loginPage.invalidLogIn(login, invalidPassword);
+        loginPage.blockingMessage();
     }
 
     @Test
     void shouldSubmittingEmptyForm() {
         val loginPage = new LoginPage();
-        loginPage.emptyLogin();
+        loginPage.emptyLogIn();
     }
 
     @Test
     void shouldSendingEmptyCode() {
         val loginPage = new LoginPage();
-        val verificationPage = loginPage.validLogin();
+        val verificationPage = loginPage.validLogIn(login, password);
         verificationPage.emptyVerify();
     }
 
     @Test
     void shouldEnterInvalidCode() {
         val loginPage = new LoginPage();
-        val verificationPage = loginPage.validLogin();
-        val invalidVerificationCode = DataHelper.getInvalidVerificationCode();
-        verificationPage.invalidVerify(invalidVerificationCode);
+        val verificationPage = loginPage.validLogIn(login, password);
+        verificationPage.invalidVerify(DbInteraction.getInvalidCode());
     }
 
     @Test
     void shouldUnregisteredUserLogin() {
         val loginPage = new LoginPage();
-        loginPage.invalidLogin();
+        loginPage.invalidLogIn(invalidLogin, invalidPassword);
     }
 }
 
